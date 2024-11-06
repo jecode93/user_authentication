54a760881c962bcbf9ed09ed31b8b090842aa876
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-11-06 09:11:51 -0500

    Authentication page.

---
+++ lib/widgets/AuthPage.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class AuthPage extends StatefulWidget {
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  final SupabaseClient supabase = Supabase.instance.client;
+
+  String? _userId;
+
+  @override
+  void initState() {
+    super.initState();
+
+    supabase.auth.onAuthStateChange.listen((data) {
+      setState(() {
+        _userId = data.session?.user.id;
+      });
+    });
+  }
+
+  Future<void> signInWithGoogle() async {
+    /// TODO: update the Web client ID with your own.
+    ///
+    /// Web Client ID that you registered with Google Cloud.
+    const webClientId =
+        '689552638097-m5eob1cfpv55sht73b5n49isvl6b9ecn.apps.googleusercontent.com';
+
+    /// TODO: update the iOS client ID with your own.
+    ///
+    /// iOS Client ID that you registered with Google Cloud.
+    const iosClientId =
+        '689552638097-1e632olufff5743rpdpv3je8ju1bio3f.apps.googleusercontent.com';
+
+    // Google sign in on Android will work without providing the Android
+    // Client ID registered on Google Cloud.
+
+    final GoogleSignIn googleSignIn = GoogleSignIn(
+      clientId: iosClientId,
+      serverClientId: webClientId,
+    );
+    final googleUser = await googleSignIn.signIn();
+    final googleAuth = await googleUser!.authentication;
+    final accessToken = googleAuth.accessToken;
+    final idToken = googleAuth.idToken;
+
+    if (accessToken == null) {
+      throw 'No Access Token found.';
+    }
+    if (idToken == null) {
+      throw 'No ID Token found.';
+    }
+
+    await supabase.auth.signInWithIdToken(
+      provider: OAuthProvider.google,
+      idToken: idToken,
+      accessToken: accessToken,
+    );
+  }
+
+  // Future<void> signInWithFacebook() async {
+  //   try {
+  //     await supabase.auth.signInWithOAuth(OAuthProvider.facebook);
+  //     print("Facebook sign-in successful!");
+  //   } catch (e) {
+  //     print("Error during Facebook sign-in: $e");
+  //   }
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text(
+          "Supabase Authentication",
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_userId != null
+                ? 'User signed in successfully with GOOGLE'
+                : 'User not signed in'),
+            const SizedBox(
+              height: 15.0,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.redAccent,
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  )),
+              onPressed: signInWithGoogle,
+              child: const Text("Sign in with Google"),
+            ),
+            const SizedBox(
+              height: 15.0,
+            ),
+            // ElevatedButton(
+            //   style: ElevatedButton.styleFrom(
+            //       backgroundColor: Colors.blueAccent,
+            //       foregroundColor: Colors.white,
+            //       textStyle: const TextStyle(
+            //         fontWeight: FontWeight.bold,
+            //       )),
+            //   onPressed: signInWithFacebook,
+            //   child: const Text("Sign in with Facebook"),
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
